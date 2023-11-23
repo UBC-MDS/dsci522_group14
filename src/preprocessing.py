17434114bf01b6816997a33f6877d55f011b703e
jlee2843 <jenny.jeeun@gmail.com>
2023-11-22 21:54:40 -0800

    reorganized files and finished tests for preprocessing steps

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,36 +1,88 @@
 import pandas as pd
 import numpy as np
 
+# Acknowledgement:
+# The format of the function documentations are derived from the sample project provided. 
+# Proper citation to the sample projct is included in `notebooks/fraud_detection.ipynb`. 
+# https://github.com/ttimbers/demo-tests-ds-analysis-python/blob/main/src/count_classes.py
+
 def count_unique_numbers(df, feature_list):
-      """_summary_
+    """
+    Tallies the count of distinct observations for each designated feature. 
 
-      Args:
-          df (_type_): _description_
-          feature_list (_type_): _description_
+    Outputs a new DataFrame containing two columns: one indicating the feature names
+    and the other representing the count of unique observations for each respective feature.
 
-      Returns:
-          _type_: _description_
-      """
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        The input DataFrame containing the data to analyze.
+    feature_list : list
+        The list of categorical features that will be analyzed. 
+
+    Returns:
+    --------
+    pandas.DataFrame
+        Dataframe with a list of categorical features and the unique number of observations per feature. 
+
+    Examples:
+    --------
+    >>> import pandas as pd
+    >>> data = pd.read_pickle('data/transactions.pkl.zip', compression="infer")
+    >>> result = count_unique_numbers(data, ["merchantName", "acqCountry", "isFraud"])
+    >>> print(result)
+
+    """
       
-      col_name = []
-      counts = []
-      for feature in feature_list:
-            col_name.append(feature)
-            counts.append(len(df[feature].unique().tolist()))
-      return pd.DataFrame({"feature":col_name, 
-                              "unique_entry_counts": counts}
-                              ).sort_values(by="unique_entry_counts", ascending=False)
+    col_name = []
+    counts = []
+    for feature in feature_list:
+        col_name.append(feature)
+        counts.append(len(df[feature].unique().tolist()))
+
+    return pd.DataFrame({"feature":col_name, 
+                            "unique_entry_counts": counts}
+                            ).sort_values(by="unique_entry_counts", ascending=False)
+
 
 def generalize_categories(column, df):
-      count_df = pd.DataFrame(df[column].value_counts())
-      count_df["frequency"] = (count_df["count"] / count_df["count"].sum())
-      count_df = count_df.drop(["count"], axis=1)
-      count_freq = count_df.to_dict()["frequency"]
+    """
+    Evaluates the occurrence frequency of each observation for categorical features. 
+    Computes the frequency, ranging from 0 (no occurrences) to 1 (all observations are identical).
 
-      origin_df = df.copy()
-      origin_df["frequency"] = origin_df[column].map(count_freq)
-      organize_counts = lambda row: "Other" if row["frequency"] <= 0.01 else row[column]
-      origin_df[column] = origin_df.apply(organize_counts, axis=1)
-      origin_df[column] = origin_df[column].astype("str")
-      
-      return origin_df
+    Parameters:
+    -----------
+    column : str
+        The name of the feature used to determine frequency of occurence.
+    df : list
+        The input DataFrame containing the data to analyze.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        The original DataFrame now incorporates an extra column labeled 'frequency.' 
+        This newly added column, specifically for the designated feature, provides details 
+        about the occurrence frequency of the corresponding observations.
+
+    Examples:
+    --------
+    >>> import pandas as pd
+    >>> data = pd.read_pickle('data/transactions.pkl.zip', compression="infer")
+    >>> result = generalize_categories("merchantName", data)
+    >>> print(result)
+
+    """
+
+    count_df = pd.DataFrame(df[column].value_counts())
+    count_df.columns = ["count"]
+    count_df["frequency"] = (count_df["count"] / count_df["count"].sum())
+    count_df = count_df.drop(["count"], axis=1)
+    count_freq = count_df.to_dict()["frequency"]
+
+    origin_df = df.copy()
+    origin_df["frequency"] = origin_df[column].map(count_freq)
+    organize_counts = lambda row: "Other" if row["frequency"] <= 0.01 else row[column]
+    origin_df[column] = origin_df.apply(organize_counts, axis=1)
+    origin_df[column] = origin_df[column].astype("str")
+    
+    return origin_df
--- tests/preprocessing.py
+++ tests/preprocessing.py
@@ -1,25 +1,60 @@
 import pandas as pd
+from pandas.testing import assert_frame_equal
 import pytest
 import sys
 import os
 
-import sys
-sys.path.append('../src')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from src.preprocessing import count_unique_numbers, generalize_categories
 
 # Import sample dataset
-sample_df = pd.read_csv("sample_df.csv")
-feature_list = ["", "", ""]
+sample_df = pd.read_csv("data/sample_df.csv")
+feature_list = ["merchantName", "acqCountry", "merchantCity", "isFraud"]
 
-def test_correct_number_count():
-    expected = count_unique_numbers(sample_df, feature_list)
+# Observed output for `count_unique_numbers`
+subset_data = pd.DataFrame({
+        "merchantName": len(sample_df["merchantName"].unique().tolist()),
+        "acqCountry": len(sample_df["acqCountry"].unique().tolist()),
+        "merchantCity": len(sample_df["merchantCity"].unique().tolist()),
+        "isFraud": len(sample_df["isFraud"].unique().tolist())
+    }, index=["unique_entry_counts"]).T.reset_index()
+subset_data.columns = ["feature", "unique_entry_counts"]
+subset_data = subset_data.sort_values(by="unique_entry_counts", ascending=False)
 
-    observed = pd.DataFrame({
-        ""
+mock_data = pd.DataFrame({
+        "feature1": [1, 1, 1, 3, 4, 5],
+        "feature2": [1, 2, 3, 4, 5, 6],
+        "feature3": ["yes", "yes", "no", "yes", "neutral", "yes"]
     })
 
-# Test for correct return type
-def test_count_classes_returns_dataframe():
-    result = count_classes(two_classes_3_obs, 'class_labels')
-    assert isinstance(result, pd.DataFrame), "count_classes` should return a pandas data frame"
+# Test if the second column is ordered in a descending order for `count_unique_numbers`
+def test_correct_number_count():
+    expected = count_unique_numbers(sample_df, feature_list)
+    assert expected['unique_entry_counts'].is_monotonic_decreasing
+
+# Test for correct return type for `count_unique_numbers`
+def test_correct_return_type():
+    expected = count_unique_numbers(sample_df, feature_list)
+    assert isinstance(expected, pd.DataFrame), "Returning incorrect data type; must be a Pandas dataframe."
+
+# Test if the observed and expected data frames are equal for `count_unique_numbers`
+def test_equal_dataframe():
+    expected = count_unique_numbers(sample_df, feature_list)
+    pd.testing.assert_frame_equal(expected, subset_data)
+
+# Test if the frequency column is created in returned output for `generealize_categories`
+def test_correct_frequency_column():
+    expected1 = generalize_categories("feature1", mock_data)
+    assert "frequency" in expected1.columns.tolist()
+
+# Test for correct number of returned rows for `generealize_categories`
+def test_number_of_rows():
+    expected1 = generalize_categories("feature1", mock_data)
+    assert expected1.shape[0] == mock_data.shape[0]
+
+# Test if correct number of frequency per observation is computed for `generealize_categories`
+def test_correct_frequency_column():
+    expected1 = generalize_categories("feature1", mock_data)
+    assert expected1["frequency"].tolist() == [0.5, 0.5, 0.5, 1/6, 1/6, 1/6]
+    
