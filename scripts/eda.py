cdb9028c28e6b61f5322087c37a705160d0f7cdc
shawnhu444 <109032936+shawnhu444@users.noreply.github.com>
2023-12-03 19:21:27 -0800

    eda.py update

--- scripts/eda.py
+++ scripts/eda.py
@@ -4,7 +4,7 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 import click 
-
+##
 
 @click.command()
 @click.option('--df-path', type=str, help="Path to get the downloaded dataset")
